a879f79682db56c9bac99847b349c20584c7ae95
taraskz <158589585+taraskz@users.noreply.github.com>
2024-05-29 03:22:58 +0000

    made the names for each location on the map(temporary)

---
+++ game_map.py
@@ -0,0 +1,6 @@
+from tabulate import tabulate
+
+tile = ["Outscirts of Catville", "Catville City", "Catlands",
+       "Mount Caterest", "Lake MacCatzie", "Catanna Desert",
+       "Catpagne Beach", "Cat-Street Mall", "Cat Ruins"]
+
