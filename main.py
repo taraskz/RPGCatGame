bd4f9d140e17fce796d6c2d47c95e9db39f1d3dd
taraskz <158589585+taraskz@users.noreply.github.com>
2024-06-07 04:35:57 +0000

    made a change to rooms to include enemies in them

--- main.py
+++ main.py
@@ -33,24 +33,28 @@ player_cats = []
 
 
 rooms = {
-    "Outskirts of Catville": "You are in the starting area of the game, \
-here you can get your first cat companion if you manage to defeat an enemy",
-    "Catlands": "You are in the lands of cats, here is where most non \
-combat cats live in peace",
-    "Catville City": "You are in the central city, it's been taken over \
-by Clawmancer Felisar's goons, be careful to not get spotted",
-    "Lake MacCatzie": "You are in front of a beatiful lake, there are \
-catfish everywhere",
-    "Mount Caterest": "In front of you is the biggest mountain in the \
-catverse, no one has every been able to climb it",
-    "Catpagne Beach": "You are at a warm beach, no enemies managed to \
-get here yet, you are safe for now",
-    "Catana Desert": "Now you are in a desert, its hot and humid in here \
-staying for too long would be bad",
-    "Cat-Street Mall": "You have made it to the shopping center, here \
-you can buy anything your hearts desires",
-    "Cat Ruins": "You have finally made to to the lair of Clawmancer Felisar \
-hopefully you are strong enough to defeat him this time. GOOD LUCK!!!"
+    "Outskirts of Catville": {"description:" "You are in the starting area \
+of the game, here you can get your first cat companion if you manage to \
+defeat an enemy" "enemy": None},
+    "Catlands": {"description:" "You are in the lands of cats, here is \
+where most non combat cats live in peace" "enemy": Enemy("Goon", 20, \
+"None", "Hiss", "Nothing", "Just some easy goons to kill.", 10)},
+    "Catville City": {"description:""You are in the central city, it's been \
+taken over by Clawmancer Felisar's goons, be careful to not get spotted"},
+    "Lake MacCatzie": {"description:" "You are in front of a beatiful lake, \
+there are catfish everywhere" "enemy": None},
+    "Mount Caterest": {"description:" "In front of you is the biggest \
+mountain in the catverse, no one has every been able to climb it\
+" "enemy": None},
+    "Catpagne Beach": {"description:" "You are at a warm beach, no enemies \
+managed to get here yet, you are safe for now" "enemy": None},
+    "Catana Desert": {"description:" "Now you are in a desert, its hot \
+and humid in here staying for too long would be bad" "enemy": None},
+    "Cat-Street Mall": {"description:" "You have made it to the shopping \
+center, here you can buy anything your hearts desires" "enemy": None},
+    "Cat Ruins": {"description:" "You have finally made to to the lair of \
+Clawmancer Felisar hopefully you are strong enough to defeat him this time. \
+GOOD LUCK!!!" "enemy": None},
 }
 
 
@@ -112,8 +116,6 @@ def battle():
     is in and they are forced to fight them, if they lose the game ends if 
     they win they get a reward
     '''
-    enemy = Enemy("Goon", 20, "None", "Hiss", "Nothing", "Just some easy goons to \
-    kill.", 10)
     while player.is_alive() and enemy.is_alive():
         print(f"Enemy : {enemy.name} (Health: {enemy.health})")
         print(f"Your cats: {[cat.name for cat in player_cats]}")
